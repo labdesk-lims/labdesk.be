52c773e145b2a4bf2f3280e75e91276c6812872d
Lutz Kogel <77008074+KOLUMA-HUB@users.noreply.github.com>
2022-05-06 08:41:42 +0200

    Create labdesk_init_server.sql

---
+++ labdesk-security_patch.sql
@@ -0,0 +1,16 @@
+CREATE TRIGGER prevent_login
+ON ALL SERVER WITH EXECUTE AS 'sa'
+FOR LOGON
+AS
+BEGIN
+	DECLARE @LoginName sysname
+	DECLARE @app nvarchar(255)
+
+	SET @app = (SELECT app_name())
+	SET @LoginName = ORIGINAL_LOGIN()
+
+	IF @app != 'labdesk-ui' And (SELECT IS_SRVROLEMEMBER('sysadmin', @LoginName)) != 1
+	BEGIN
+		ROLLBACK; --Disconnect the session
+	END
+END
