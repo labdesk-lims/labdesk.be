24d2f82e288d478ad30658556a0284fd45d5fea1
Lutz Kogel <77008074+KOLUMA-HUB@users.noreply.github.com>
2023-08-07 16:28:29 +0200

    Optimizations
    Performance Optimizations

--- labdesk-security_patch.sql
+++ labdesk-security_patch.sql
@@ -5,11 +5,13 @@ AS
 BEGIN
 	DECLARE @LoginName sysname
 	DECLARE @app nvarchar(255)
+	DECLARE @db_name nvarchar(128)
 
+	SET @db_name = DB_NAME()
 	SET @app = (SELECT app_name())
 	SET @LoginName = ORIGINAL_LOGIN()
 
-	IF @app != 'labdesk-ui' And (SELECT IS_SRVROLEMEMBER('sysadmin', @LoginName)) != 1
+	IF @app != 'labdesk-ui' And @db_name = 'labdesk' And (SELECT IS_SRVROLEMEMBER('sysadmin', @LoginName)) != 1
 	BEGIN
 		ROLLBACK; --Disconnect the session
 	END
